2b83caa090c1087b3a4705708b1075929f52434e
muhammaddanihaikal <dainnaxjakarta91@gmail.com>
2026-08-06 17:16:27 +0700

    refactor(tests): simplify comments in test_login2.py

--- tests/test_login2.py
+++ tests/test_login2.py
@@ -2,17 +2,17 @@ from playwright.sync_api import expect
 from pages.login_page import LoginPage
 from utils.read_data import read_data
 
+# Load data pengujian dari JSON
 login_data = read_data("login_data.json")
 
-def test_login_valid(page):
-    """"Melakukan login menggunakan data yang valid"""
-    # ambil data
-    username = login_data["valid_login"]["username"]
-    password = login_data["valid_login"]["password"]
 
-    # buat object login page
+def test_login_valid(page):
+    """Pengujian login dengan data kredensial valid."""
+    data = login_data["valid_login"]
+
+    # Buka halaman dan lakukan login
     login_page = LoginPage(page)
-    # masuk ke halaman login
     login_page.open()
-    # melakukan login
-    login_page.login(username, password)
+    login_page.login(data["username"], data["password"])
+
+
