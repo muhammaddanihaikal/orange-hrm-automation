69b043eadc333cdcba068fded8973eae6243e16c
muhammaddanihaikal <dainnaxjakarta91@gmail.com>
2026-09-09 11:25:18 +0700

    feat(fixture): tambahkan fixture api_create_bulk_users untuk pengujian bulk action

--- conftest.py
+++ conftest.py
@@ -1,8 +1,10 @@
-import pytest
-from playwright.sync_api import sync_playwright, Browser, Page
-from config import HEADLESS, BASE_URL
-from pages.login_page import LoginPage
 import os
+
+import pytest
+from playwright.sync_api import Browser, Page, sync_playwright
+
+from config import BASE_URL, HEADLESS
+from pages.login_page import LoginPage
 
 
 @pytest.fixture(scope="session")
@@ -15,10 +17,7 @@ def playwright():
 @pytest.fixture(scope="session")
 def browser(playwright):
     """Membuka browser (Chromium) sekali untuk seluruh sesi test."""
-    browser = playwright.chromium.launch(
-        headless=HEADLESS,
-        slow_mo=0
-    )
+    browser = playwright.chromium.launch(headless=HEADLESS, slow_mo=0)
 
     yield browser
 
@@ -77,13 +76,14 @@ def logged_in_page(browser: Browser, global_login):
     # buka context baru, TAPI langsung pake cookie dari state.json
     context = browser.new_context(storage_state=".auth/state.json")
     page = context.new_page()
-    
+
     # Langsung arahkan ke dashboard supaya test tinggal pakai
     page.goto(f"{BASE_URL}/web/index.php/dashboard/index")
 
     yield page
 
     context.close()
+
 
 @pytest.fixture
 def api_create_user(logged_in_page: Page):
@@ -94,12 +94,12 @@ def api_create_user(logged_in_page: Page):
     post_response = logged_in_page.request.post(
         f"{BASE_URL}/web/index.php/api/v2/admin/users",
         data={
-            "userRoleId": 1, 
+            "userRoleId": 1,
             "empNumber": 2,
-            "username": tumbal_username, 
-            "password": "JagungManis_9192", 
-            "status": True 
-        }
+            "username": tumbal_username,
+            "password": "JagungManis_9192",
+            "status": True,
+        },
     )
     assert post_response.ok, f"Gagal bikin user via API: {post_response.text()}"
 
@@ -111,12 +111,50 @@ def api_create_user(logged_in_page: Page):
 
     # 3. teardown (hapus user)
     delete_response = logged_in_page.request.delete(
-        f"{BASE_URL}/web/index.php/api/v2/admin/users",
-        data={
-            "ids": [user_id]
-        }
+        f"{BASE_URL}/web/index.php/api/v2/admin/users", data={"ids": [user_id]}
     )
 
     # abaikan kalo statusnya 404, karena tujuanya udah tercapai yaitu "User Terhapus"
-    if(delete_response.status != 404):
+    if delete_response.status != 404:
         assert delete_response.ok, f"Gagal hapus user via API: {delete_response.text()}"
+
+
+@pytest.fixture
+def api_create_bulk_users(logged_in_page: Page):
+    """Menyiapkan 2 user data via API untuk pengujian bulk action dan menghpusnya seletah test"""
+    prefix = "bulk_user_"
+    usernames = [f"{prefix}1", f"{prefix}2"]
+    created_user_ids = []
+
+    # 1. Setup: buat 2 user via API
+    for username in usernames:
+        post_response = logged_in_page.request.post(
+            f"{BASE_URL}/web/index.php/api/v2/admin/users",
+            data={
+                "userRoleId": 1,
+                "empNumber": 2,
+                "username": username,
+                "password": "JagungManis_9192",
+                "status": True,
+            },
+        )
+
+        assert post_response.ok, (
+            f"Gagal bikin user {username} via API: {post_response.text()}"
+        )
+        user_id = post_response.json()["data"]["id"]
+        created_user_ids.append(user_id)
+
+    # 2. Pinjamkan prefix dan list username ke fungsi test
+    yield {"prefix": prefix, "usernames": usernames}
+
+    # 3. Teardown: Hapus kedua user via API
+    delete_response = logged_in_page.request.delete(
+        f"{BASE_URL}/web/index.php/api/v2/admin/users",
+        data={"ids": created_user_ids},
+    )
+
+    if delete_response.status != 404:
+        assert delete_response.ok, (
+            f"Gagal hapus bulk user via API: {delete_response.text()}"
+        )
